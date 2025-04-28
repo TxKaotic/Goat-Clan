761f9c39efed0a65b3749bd692a80d2377868672
TxKaotic <daniels.stephen2011@yahoo.com>
2025-04-28 13:47:22 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+const modalContent = document.querySelector(".modal-content");
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+  if (document.getElementById('squireReq1').checked && 
+      document.getElementById('squireReq2').checked) {
+    setRank("Squire")
+  }
+  else if ( document.getElementById('expertReq1').checked &&
+            document.getElementById('expertReq3').checked &&
+            document.getElementById('expertReq3').checked) {
+    setRank("Expert")
+  }
+  else if ( document.getElementById('knightReq1').checked &&
+            document.getElementById('knightReq3').checked &&
+            document.getElementById('knightReq3').checked) {
+    setRank("Knight")
+  }   
+  else if ( document.getElementById('paladinReq1').checked &&
+            document.getElementById('paladinReq3').checked &&
+            document.getElementById('paladinReq3').checked) {
+    setRank("Paladin")
+  }
+  else if ( document.getElementById('astralReq1').checked &&
+            document.getElementById('astralReq2').checked) {
+    setRank("Astral")
+  }  
+  
+  
+    
+  else modalContent.textContent = "Make sure you only check the boxes for a section you have the reqs for!"
+};
+
+const setRank = function(rank){
+  
+  modalContent.textContent = (`Welcome to the GOAT Clan. Your rank: ` + rank)
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+

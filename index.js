f6dd3695957887236eeff5f631eed44c1a3ed547
TxKaotic <daniels.stephen2011@yahoo.com>
2025-04-28 17:25:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const modalContent = document.querySelector(".modal-content");
 
-
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -13,39 +12,43 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
-  if (document.getElementById('squireReq1').checked && 
-      document.getElementById('squireReq2').checked) {
-    setRank("Squire")
-  }
-  else if ( document.getElementById('expertReq1').checked &&
-            document.getElementById('expertReq3').checked &&
-            document.getElementById('expertReq3').checked) {
-    setRank("Expert")
-  }
-  else if ( document.getElementById('knightReq1').checked &&
-            document.getElementById('knightReq3').checked &&
-            document.getElementById('knightReq3').checked) {
-    setRank("Knight")
-  }   
-  else if ( document.getElementById('paladinReq1').checked &&
-            document.getElementById('paladinReq3').checked &&
-            document.getElementById('paladinReq3').checked) {
-    setRank("Paladin")
-  }
-  else if ( document.getElementById('astralReq1').checked &&
-            document.getElementById('astralReq2').checked) {
-    setRank("Astral")
-  }  
-  
-  
-    
-  else modalContent.textContent = "Make sure you only check the boxes for a section you have the reqs for!"
+  let inGameName = document.querySelector("#ign").value;
+  if (
+    document.getElementById("squireReq1").checked &&
+    document.getElementById("squireReq2").checked
+  ) {
+    setRank("Squire");
+  } else if (
+    document.getElementById("expertReq1").checked &&
+    document.getElementById("expertReq3").checked &&
+    document.getElementById("expertReq3").checked
+  ) {
+    setRank("Expert");
+  } else if (
+    document.getElementById("knightReq1").checked &&
+    document.getElementById("knightReq3").checked &&
+    document.getElementById("knightReq3").checked
+  ) {
+    setRank("Knight");
+  } else if (
+    document.getElementById("paladinReq1").checked &&
+    document.getElementById("paladinReq3").checked &&
+    document.getElementById("paladinReq3").checked
+  ) {
+    setRank("Paladin");
+  } else if (
+    document.getElementById("astralReq1").checked &&
+    document.getElementById("astralReq2").checked
+  ) {
+    setRank("Astral");
+  } else
+    modalContent.textContent = `Welcome, ${inGameName}, make sure you only check the boxes for a section you have the reqs for!`;
 };
 
-const setRank = function(rank){
-  
-  modalContent.textContent = (`Welcome to the GOAT Clan. Your rank: ` + rank)
-}
+const setRank = function (rank) {
+  let inGameName = document.querySelector("#ign").value;
+  modalContent.textContent = `Welcome, ${inGameName}. Your rank is: ${rank}`;
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
@@ -58,4 +61,3 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
-
